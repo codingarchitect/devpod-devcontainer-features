50feb5b5a1804fa7a7e13fe6bde1a031396ecc71
Sendhil Kumar R <sendhil.r@gmail.com>
2024-07-04 03:51:46 +0000

    Added a redis sample to test devpod devcontainer features

---
+++ DevPodDevContainerFeature.HelloRedis/DevPodDevContainerFeature.HelloRedis/Program.cs
@@ -0,0 +1,6 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+var db = redis.GetDatabase();
+db.StringSet("greeting", "Hello from redis!");
+Console.WriteLine(db.StringGet("greeting"));
